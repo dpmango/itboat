dfd4269525b8d0527c0d9e741e20cef60ec6df3a
cssio <vlad.muraviov@gmail.com>
2017-11-16 23:22:34 +0200

    Edit

--- src/js/main.js
+++ src/js/main.js
@@ -454,6 +454,7 @@ $(document).ready(function(){
             items: 1,
             margin: 102,
             lazyLoad: true,
+            autoHeight: true,
             smartSpeed: galleryFullDuration,
             fluidSpeed: galleryFullDuration,
             navText: [iconPrev,iconNext],
@@ -1204,69 +1205,69 @@ $(document).ready(function(){
   ////////////
 
   // custom selects
-  $('.ui-select__visible').on('click', function(e){
-    var that = this
-    // hide parents
-    $(this).parent().parent().parent().find('.ui-select__visible').each(function(i,val){
-      if ( !$(val).is($(that)) ){
-        $(val).parent().removeClass('active')
-      }
-    });
+  // $('.ui-select__visible').on('click', function(e){
+  //   var that = this
+  //   // hide parents
+  //   $(this).parent().parent().parent().find('.ui-select__visible').each(function(i,val){
+  //     if ( !$(val).is($(that)) ){
+  //       $(val).parent().removeClass('active')
+  //     }
+  //   });
 
-    $(this).parent().toggleClass('active');
-  });
+  //   $(this).parent().toggleClass('active');
+  // });
 
-  $('.ui-select__dropdown span').on('click', function(){
-    // parse value and toggle active
-    var value = $(this).data('val');
-    if (value){
-      $(this).siblings().removeClass('active');
-      $(this).addClass('active');
+  // $('.ui-select__dropdown span').on('click', function(){
+  //   // parse value and toggle active
+  //   var value = $(this).data('val');
+  //   if (value){
+  //     $(this).siblings().removeClass('active');
+  //     $(this).addClass('active');
 
-      // set visible
-      $(this).closest('.ui-select').removeClass('active');
-      $(this).closest('.ui-select').find('input').val(value);
+  //     // set visible
+  //     $(this).closest('.ui-select').removeClass('active');
+  //     $(this).closest('.ui-select').find('input').val(value);
 
-      $(this).closest('.ui-select').find('.ui-select__visible span').text(value);
-    }
+  //     $(this).closest('.ui-select').find('.ui-select__visible span').text(value);
+  //   }
 
-  });
+  // });
 
-  // handle outside click
-  $(document).click(function (e) {
-    var container = new Array();
-    container.push($('.ui-select'));
+  // // handle outside click
+  // $(document).click(function (e) {
+  //   var container = new Array();
+  //   container.push($('.ui-select'));
 
-    $.each(container, function(key, value) {
-        if (!$(value).is(e.target) && $(value).has(e.target).length === 0) {
-            $(value).removeClass('active');
-        }
-    });
-  });
+  //   $.each(container, function(key, value) {
+  //       if (!$(value).is(e.target) && $(value).has(e.target).length === 0) {
+  //           $(value).removeClass('active');
+  //       }
+  //   });
+  // });
 
-  // numeric input
-  $('.ui-number span').on('click', function(e){
-    var element = $(this).parent().find('input');
-    var currentValue = parseInt($(this).parent().find('input').val()) || 0;
+  // // numeric input
+  // $('.ui-number span').on('click', function(e){
+  //   var element = $(this).parent().find('input');
+  //   var currentValue = parseInt($(this).parent().find('input').val()) || 0;
 
-    if( $(this).data('action') == 'minus' ){
-      if(currentValue <= 1){
-        return false;
-      }else{
-        element.val( currentValue - 1 );
-      }
-    } else if( $(this).data('action') == 'plus' ){
-      if(currentValue >= 99){
-        return false;
-      } else{
-        element.val( currentValue + 1 );
-      }
-    }
-  });
+  //   if( $(this).data('action') == 'minus' ){
+  //     if(currentValue <= 1){
+  //       return false;
+  //     }else{
+  //       element.val( currentValue - 1 );
+  //     }
+  //   } else if( $(this).data('action') == 'plus' ){
+  //     if(currentValue >= 99){
+  //       return false;
+  //     } else{
+  //       element.val( currentValue + 1 );
+  //     }
+  //   }
+  // });
 
-  // Masked input
-  $(".js-dateMask").mask("99.99.99",{placeholder:"ДД.ММ.ГГ"});
-  $("input[type='tel']").mask("+7 (000) 000-0000", {placeholder: "+7 (___) ___-____"});
+  // // Masked input
+  // $(".js-dateMask").mask("99.99.99",{placeholder:"ДД.ММ.ГГ"});
+  // $("input[type='tel']").mask("+7 (000) 000-0000", {placeholder: "+7 (___) ___-____"});
 
 
 
